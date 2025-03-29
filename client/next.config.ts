2b4d206bc663b92719cea09f0e07dfab952f61a0
Alok <mishraalok189381@gmail.com>
2025-03-29 18:40:51 +0530

    next config

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,17 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // Ensures API routes work correctly
+  reactStrictMode: true, // Optional, but helps catch errors
 };
 
 export default nextConfig;
