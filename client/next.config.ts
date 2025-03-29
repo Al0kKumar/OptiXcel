0f7a76977ce0ddf028d3b070ffd1b2d47f309ff2
Alok <mishraalok189381@gmail.com>
2025-03-29 22:26:03 +0530

    env updated

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,9 +1,7 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  reactStrictMode: true, // Improves debugging
+  output: "standalone", // Ensures proper deployment
 };
 
 module.exports = nextConfig;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -18,6 +18,8 @@ export default function Home() {
     }
   };
 
+  console.log("env is ", `${process.env.NEXT_PUBLIC_API_URL}`);
+
   const comp = 123;
   const conv = 59;
 
